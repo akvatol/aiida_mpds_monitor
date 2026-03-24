1f3175242cde9ecb9968387b57eed1a338330fbc
Anton Domnin <a.v.daomnin@gmail.com>
2026-03-24 21:18:01 +0000

    add more waiting states

--- aiida_mpds_monitor/status.py
+++ aiida_mpds_monitor/status.py
@@ -70,7 +70,7 @@ def get_node_status(node, child_types=None, logger=None):
             return f"{STATUS_EXC}-{exit_code}"
         return STATUS_EXC
 
-    elif state.lower() in ["running", "submitting", "created"]:
+    elif state.lower() in ["running", "submitting", "created", "waiting", "paused"]:
         return STATUS_WAITING
 
     elif state.lower() == "excepted":
