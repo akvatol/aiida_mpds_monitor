ed87f313e361cb7adf1f5b9e0e414e3d128dc123
Anton Domnin <a.v.daomnin@gmail.com>
2026-03-27 14:00:49 +0000

    fix path

--- aiida_mpds_monitor/config.py
+++ aiida_mpds_monitor/config.py
@@ -6,7 +6,7 @@ import yaml
 from aiida.common.extendeddicts import AttributeDict
 
 
-DEFAULT_CONFIG_PATH = Path.home() / ".aiida" / "aiida_mpds_monitor"
+DEFAULT_CONFIG_PATH = Path.home() / ".aiida" / "aiida_mpds_monitor" / "conf.yaml"
 
 DEFAULT_CONFIG = {
     "webhook_url": "http://localhost:8080",
